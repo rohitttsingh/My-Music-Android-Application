b22bd65eb9644c11d2496e884570dd672ecf00af
Rohit270701 <rohitsingh270701@gmail.com>
2022-02-19 22:50:28 +0530

    Version 1.0

---
+++ app/src/main/java/com/example/musicmy/MyMusic.java
@@ -0,0 +1,70 @@
+package com.example.musicmy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MyMusic extends AppCompatActivity implements View.OnClickListener {
+ImageButton btn_pause , btn_play , btn_stop ;
+Button   btn_next ;
+MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_music);
+
+        btn_pause = findViewById(R.id.pause);
+        btn_play= findViewById(R.id.play);
+        btn_stop = findViewById(R.id.stop);
+        //btn_previous= findViewById(R.id.previous);
+        btn_next = findViewById(R.id.next);
+
+        btn_stop.setOnClickListener(this);
+        btn_play.setOnClickListener(this);
+        btn_pause.setOnClickListener(this);
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyMusic.this,MYMUSICSSS.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case  R.id.play:
+                mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.music);
+                mediaPlayer.start();
+                Toast.makeText(MyMusic.this,"MUSIC PLAYED",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.pause:
+               // if (mediaPlayer!=null) {
+                    mediaPlayer.pause();
+                Toast.makeText(MyMusic.this,"MUSIC PAUSED",Toast.LENGTH_SHORT).show();
+              //  }
+                break;
+            case R.id.stop:
+              //  if(mediaPlayer!=null) {
+                    mediaPlayer.stop();
+                Toast.makeText(MyMusic.this,"MUSIC STOPPED",Toast.LENGTH_SHORT).show();
+             //   }
+                break;
+
+
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
